9c25e7261139987a11e38111db6b2f1ff816ec9f
Bin HU <hubinitp@gmail.com>
2019-03-26 15:42:17 +0800

    boundaries

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import healpy as hp
 import matplotlib.pyplot as plt
-import pyfits
+#import pyfits
 #from astropy.io import fits 
 #import astropy
 
@@ -13,8 +13,100 @@ map_I = hp.read_map('../PlanckData/COM_CMB_IQU-nilc_1024_R2.02_full.fits',field=
 
 #hp.fitsfunc.read_map('./PlanckData/COM_CMB_IQU-nilc_1024_R2.02_full.fits',field=0,h=True)
 
-#print map_I
+nside = 1024
 
+#-----------------------------------#
+#plot the fullfilled circle
+
+#help(hp.pixelfunc.ang2vec)
+#bh: define the center of the disc
+vec=hp.pixelfunc.ang2vec(phi=0.,theta=np.pi/2)
+
+#bh: define the radial geodesic arc length
+radius = 1.0
+
+#help(hp.query_disc)
+disc_pix = hp.query_disc(nside,vec,radius,inclusive=False,fact=4,nest=False)
+
+#bh: erase the cmb info inside the disc
+map_I[disc_pix]=1.0e-4
+
+hp.mollview(map_I)
+
+plt.show()
+
+#-----------------------------------#
+#plot the unfilled circle
+
+#bh: define a small circle
+radius2 = 1.0-0.02
+
+disc_pix2 = hp.query_disc(nside,vec,radius2,inclusive=False,fact=4,nest=False)
+
+#bh: subtract the outer disc by the inner disc, get the ring
+ring_pix = np.setdiff1d(disc_pix,disc_pix2)
+
+map_I = hp.read_map('../PlanckData/COM_CMB_IQU-nilc_1024_R2.02_full.fits',field=0)
+
+#bh: erase the cmb info along the ring
+map_I[ring_pix]=-3.0e-4
+
+hp.mollview(map_I)
+
+plt.show()
+
+#-----------------------------------#
+#plot the fullfilled rectangle
+
+#bh: the definition order of the 4 vertices are important! have to be clock-wise or anticlock-wise!!!
+vert1 = hp.pixelfunc.ang2vec(phi=0.,theta=np.pi/3.)
+vert2 = hp.pixelfunc.ang2vec(phi=np.pi/3.,theta=np.pi/3.)
+vert3 = hp.pixelfunc.ang2vec(phi=np.pi/3.,theta=np.pi/2.)
+vert4 = hp.pixelfunc.ang2vec(phi=0.,theta=np.pi/2.)
+
+vertice_coord = [[vert1[0],vert1[1],vert1[2]],[vert2[0],vert2[1],vert2[2]],[vert3[0],vert3[1],vert3[2]],[vert4[0],vert4[1],vert4[2]]]
+
+rect_pix = hp.query_polygon(nside,vertice_coord,inclusive=False,fact=4,nest=False)
+
+map_I = hp.read_map('../PlanckData/COM_CMB_IQU-nilc_1024_R2.02_full.fits',field=0)
+
+map_I[rect_pix]=-2.0e-4
+
+hp.mollview(map_I)
+
+plt.show()
+
+#-----------------------------------#
+#plot the unfilled rectangle
+
+
+#bh: define the inner rectangle
+vert_s1 = hp.pixelfunc.ang2vec(phi=0.+0.02,theta=np.pi/3.+0.01)
+vert_s2 = hp.pixelfunc.ang2vec(phi=np.pi/3.-0.02,theta=np.pi/3.+0.02)
+vert_s3 = hp.pixelfunc.ang2vec(phi=np.pi/3.-0.02,theta=np.pi/2.-0.02)
+vert_s4 = hp.pixelfunc.ang2vec(phi=0.+0.02,theta=np.pi/2.-0.02)
+
+vertice_s_coord = [[vert_s1[0],vert_s1[1],vert_s1[2]],[vert_s2[0],vert_s2[1],vert_s2[2]],[vert_s3[0],vert_s3[1],vert_s3[2]],[vert_s4[0],vert_s4[1],vert_s4[2]]]
+
+rect2_pix = hp.query_polygon(nside,vertice_s_coord,inclusive=False,fact=4,nest=False)
+
+#bh: subtract the outer disc by the inner disc, get the ring
+rect_ring_pix = np.setdiff1d(rect_pix,rect2_pix)
+
+map_I = hp.read_map('../PlanckData/COM_CMB_IQU-nilc_1024_R2.02_full.fits',field=0)
+
+map_I[rect_ring_pix]=-3.0e-4
+
+hp.mollview(map_I)
+
+plt.show()
+
+quit()
+
+#-----------------------------------#
+#gnomonicProj
+
+'''
 hpg = hp.projector.GnomonicProj()
 
 def myvec2pix(x,y,z):
@@ -49,6 +141,7 @@ plt.show()
 plt.imshow(cmb_map_data)
 
 plt.show()
+'''
 
 
 '''
